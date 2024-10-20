d39566eb265c2282408a650a4bcfd72c759d9c56
ACodeReview <acodereview@gmail.com>
2024-10-19 21:18:11 -0700

    Add description and update home page to test deploy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,8 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "ACodeReview.com",
-  description: "ACodeReview.com",
+  description:
+    "ACodeReview.com is your go-to platform for expert code reviews, insightful programming tutorials, and practical advice for developers. Join us as we break down popular coding courses, identify bad habits, and offer best practices for cleaner, more efficient code. Whether you’re a beginner looking to level up your skills or an experienced developer seeking refined techniques, ACodeReview.com provides valuable content to boost your coding expertise. Explore our YouTube channel @ACodeReview for real-world code critiques and stay tuned for premium courses, guides, and resources to accelerate your coding journey.",
 };
 
 type RootLayoutProps = {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
-const Home = () => <h1>Home</h1>;
+const Home = () => (
+  <>
+    <h1>A Code Review</h1>
+    <p>
+      ACodeReview.com is your go-to platform for expert code reviews, insightful
+      programming tutorials, and practical advice for developers.
+    </p>
+  </>
+);
 
 export default Home;
